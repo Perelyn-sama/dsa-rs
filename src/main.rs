1ebb347e5a097ed988a440556e5eb287d97e5758
Perelyn-sama <okohebina@gmail.com>
2022-10-04 00:00:43 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+mod unique_email;
+mod reverse_string;
+mod armstrong_numbers;
+mod beer_song;
+mod prime_factors;
+mod differences_of_squares;
+
+fn main() {
+    println!("{:?}", unique_email::run(vec!["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]));
+
+}
+
+
+
