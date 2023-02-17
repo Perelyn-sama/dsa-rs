cf715ebd4c273a42fc25c72273b2a4b5227745de
Perelyn <pisckid344@gmail.com>
2023-02-17 23:51:35 +0100

    add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod unique_email;
+mod reverse_string;
+mod armstrong_numbers;
+mod beer_song;
+mod prime_factors;
+mod differences_of_squares;
+mod leap_year;
+mod brackets_are_balanced;
+mod build_proverb;
+mod SHA256;
+mod raindrops;
+mod sum_of_multiples;
+mod series; // not done yet
+mod grains; // not done yet
+mod clock;
+mod sublist;
